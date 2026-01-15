098d786c72c5272b73f28ebf832d2d001daa7fa7
LomyaraMax <154744731+LomyaraMax@users.noreply.github.com>
2026-01-15 20:11:13 +0200

    Update timeout for success message after form submission
    Increase timeout duration for success message visibility.

--- js/main.js
+++ js/main.js
@@ -4,13 +4,10 @@ const success = document.getElementById('success');
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  // Здесь можно подключить:
-  // fetch / Formspree / Telegram Bot / EmailJS
-
   success.classList.remove('hidden');
   form.reset();
 
   setTimeout(() => {
     success.classList.add('hidden');
-  }, 4000);
+  }, 5000);
 });
